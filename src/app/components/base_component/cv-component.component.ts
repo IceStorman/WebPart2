e3a91eaac49b8e9d95f34f9d82b9f4173575a614
IceStorman <vladgamedeveloper@gmail.com>
2025-05-07 22:43:46 +0300

    Added photo

--- src/app/components/base_component/cv-component.component.ts
+++ src/app/components/base_component/cv-component.component.ts
@@ -3,14 +3,17 @@ import {CandidateSummary} from '../candidate-summary-component/candidate-summary
 import {AboutMeComponent} from '../about-me-component/about-me-component';
 import {EducationComponent} from '../education-component/education-component';
 import {JobExperienceComponent} from '../job-experience-component/job-experience-component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'cv-component',
+  styleUrls: ["./cv-component.scss"],
   imports: [
     CandidateSummary,
     AboutMeComponent,
     EducationComponent,
-    JobExperienceComponent
+    JobExperienceComponent,
+    NgOptimizedImage
   ],
   templateUrl: './cv-component.component.html'
 })
