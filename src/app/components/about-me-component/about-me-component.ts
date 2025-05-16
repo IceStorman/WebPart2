dead4d1040b5013b7953d713301a20c10ffbc17a
IceStorman <vladgamedeveloper@gmail.com>
2025-05-16 21:22:14 +0300

    Some refactor

--- src/app/components/about-me-component/about-me-component.ts
+++ src/app/components/about-me-component/about-me-component.ts
@@ -2,6 +2,14 @@ import { Component } from "@angular/core";
 import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
+class AboutMe{
+  constructor(
+    public firstPart: string,
+    public highlightedPart: string,
+    public lastPart: string,
+  ){}
+}
+
 @Component({
   selector: "about-me-component",
   templateUrl: "./about-me-component.html",
@@ -14,4 +22,10 @@ export class AboutMeComponent {
   constructor(library: FaIconLibrary) {
     library.addIcons(faUser);
   }
+
+  aboutMe = new AboutMe(
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    "When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting."
+  );
 }
